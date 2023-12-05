7d0a6a2db056b680bb16a57a3e1b542c102ada47
Kian Parvin <kian.parvin@canonical.com>
2023-12-05 15:10:48 +0200

    linter friendly

--- src/constants/snap.py
+++ src/constants/snap.py
@@ -4,6 +4,6 @@
 """Snap names and commands constants."""
 
 SERVER_SNAP_NAME = "canonical-livepatch-server"
-SERVER_SNAP_REVISION=18
+SERVER_SNAP_REVISION = 18
 SCHEMA_UPGRADE_COMMAND = "schema-tool"
 SCHEMA_VERSION_CHECK = "check-schema-version"
