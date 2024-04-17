7b1caea97369671ca32b724bd8fa2fff786ef22d
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-04-17 15:25:05 +0100

    Remove empty test files
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- tests/integration/test_relations.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2024 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""Integration tests for relations."""
-
-import logging
-
-LOGGER = logging.getLogger(__name__)
--- tests/integration/test_schema_upgrade.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 Canonical Ltd.
-# See LICENSE file for licensing details.
-"""Schema upgrades integration tests."""
-import logging
-
-LOGGER = logging.getLogger(__name__)
