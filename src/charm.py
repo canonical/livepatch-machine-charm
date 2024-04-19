8147de4a3ae5cf694bd23eee7abfdd2c557c5d4e
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-04-19 10:10:17 +0100

    Extract required config params check into a method
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -137,16 +137,25 @@ class OperatorMachineCharm(CharmBase):
         else:
             self.set_status_and_log("Livepatch snap already installed...", WaitingStatus)
 
-    def _config_changed(self, event: Union[ConfigChangedEvent, None]):
-        """Update snap internal configuration, additionally validating the DB is ready each time."""
-        required_settings = REQUIRED_SETTINGS.copy()
+    def _check_required_config_assigned(self) -> bool:
+        """
+        Check required configuration parameters are assigned.
 
+        This will set the status as blocked if any of the required parameters
+        were not set.
+        """
+        required_settings = REQUIRED_SETTINGS.copy()
         for setting, error_msg in required_settings.items():
             if self.config.get(setting) in (None, ""):
                 self.set_status_and_log(error_msg, BlockedStatus)
                 logger.warning(error_msg)
-                return
+                return False
+        return True
 
+    def _config_changed(self, event: Union[ConfigChangedEvent, None]):
+        """Update snap internal configuration, additionally validating the DB is ready each time."""
+        if not self._check_required_config_assigned():
+            return
         if not self._check_install_and_relations():
             return
         if not self._database_migrated():
